3e245a0847ccfd3b964dbcc181fed6a7d16d8d07
frsiam <siam.frs@gmail.com>
2022-02-09 21:49:35 +0600

    apply the delete activities

--- to-do.js
+++ to-do.js
@@ -10,8 +10,17 @@ document.getElementById('add-item').addEventListener('click',function(){
         const rowContent = document.createElement('tr');
         rowContent.innerHTML = `<th scope="row">${count}</th>
         <td>${inputValue}</td>
-        <td><button class="btn btn-danger px-2 m-1">Delete</button><button class="btn btn-success px-2 m-1">Done</button></td>`;
+        <td><button class="btn btn-danger px-2 m-1 delete-btn">Delete</button><button class="btn btn-success px-2 m-1 done-btn">Done</button></td>`;
         content.appendChild(rowContent);
         document.getElementById('new-item').value = '';
+
+        const deleteButton = document.getElementsByClassName('delete-btn');
+        const doneButton = document.getElementsByClassName('done-btn');
+        //for delete
+        for(const button of deleteButton){
+            button.addEventListener('click',function(e){
+                e.target.parentNode.parentNode.style.display = "none";
+            })
+        }
     }
 })
