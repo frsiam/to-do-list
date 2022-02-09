c33f056435a9c29a5c3fdefc1982d72464d04993
frsiam <siam.frs@gmail.com>
2022-02-09 21:30:46 +0600

    action button done

--- to-do.js
+++ to-do.js
@@ -10,7 +10,7 @@ document.getElementById('add-item').addEventListener('click',function(){
         const rowContent = document.createElement('tr');
         rowContent.innerHTML = `<th scope="row">${count}</th>
         <td>${inputValue}</td>
-        <td>Otto</td>`;
+        <td><button class="btn btn-danger px-2 m-1">Delete</button><button class="btn btn-success px-2 m-1">Done</button></td>`;
         content.appendChild(rowContent);
         document.getElementById('new-item').value = '';
     }
